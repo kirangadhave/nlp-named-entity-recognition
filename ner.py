1f85feaba7f31563691f79dbdb319a073a6e8c5a
Kiran Gadhave <u1143683@utah.edu>
2017-10-24 16:09:46 -0600

    "Data Loading routine complete"

--- ner.py
+++ ner.py
@@ -17,12 +17,21 @@ def load_locations(locs_file):
 	return pd.read_csv(locs_file, header = None)[0].tolist()
 
 def load_data_from_file(train_file):
+	sentences = []
 	with open(train_file, 'r') as f:
+		words = []
 		for x in f.readlines():
 			if(x.strip() == ""):
-				print("lol")
+				if(len(words) > 0):
+					sentence = Sentence(words)
+					sentences.append(sentence)
+					words = []
 			else:
-				print(x)
+				word_arr = x.strip().split()
+				word = Word(word_arr)
+				words.append(word)
+	return sentences
+			
 if(len(sys.argv) < 5): 
 	print("Error. Only following arguments")
 	print(len(sys.argv))
@@ -36,3 +45,13 @@ ftypes = sys.argv[4:]
 locations = load_locations(locs_file)
 
 train_data = load_data_from_file(train_file)
+
+count = 5
+
+for x in train_data:
+	for y in x.words:
+		print(y.label, y.pos, y.w)
+	print("New Sent")
+	count -= 1
+	if(count == 0):
+		sys.exit()
