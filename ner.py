07708f3c37ab7cdd54d1ad3b3f80394d35687168
Kiran Gadhave <u1143683@utah.edu>
2017-10-24 15:52:59 -0600

    "Location extracted"

--- ner.py
+++ ner.py
@@ -3,13 +3,28 @@ import sys
 import numpy as np
 import pandas as pd
 
+class Word:
+	def __init__(self, word_arr):
+		self.label = word_arr[0]
+		self.pos = word_arr[1]
+		self.w = word_arr[2]
+
+class Sentence:
+	def __init__(self, words):
+		self.words = words
+
+def load_locations(locs_file):
+	return pd.read_csv(locs_file, header = None)
+
 if(len(sys.argv) < 5): 
 	print("Error. Only following arguments")
 	print(len(sys.argv))
+	sys.exit()
 
 train_file = sys.argv[1]
 test_file = sys.argv[2]
 locs_file = sys.argv[3]
 ftypes = sys.argv[4:]
 
-print(ftypes) 
+locations = load_locations(locs_file)[0].tolist()
+
