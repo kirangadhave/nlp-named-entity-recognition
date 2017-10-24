1276cc8dde29e1a6967321d6bf5f8345da088698
Kiran Gadhave <u1143683@utah.edu>
2017-10-24 15:42:05 -0600

    Update

---
+++ ner.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+if(len(sys.argv) < 5): 
+	print("Error. Only following arguments")
+	print(len(sys.argv))
+
+train_file = sys.argv[1]
+test_file = sys.argv[2]
+locs_file = sys.argv[3]
+ftypes = sys.argv[4:]
+
+print(ftypes)
